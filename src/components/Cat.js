cb023e399f5c63d81a3bb7169dc570e03c3112cc
Mark Arciaga <mark.arciaga@adadevelopersacademy.org>
2022-12-09 10:30:55 -0800

    Removed default CRA components and styles. Added Cat and Catlist components.

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cat = () => {
+  return (
+    <li>
+      <h3>Cat name</h3>
+      <button>Add Cat</button>
+    </li>
+  )
+};
+
+export default Cat;
---
+++ src/components/CatList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Cat from "./Cat";
+
+const CatList = () => {
+  return (
+    <ul>
+      <Cat />
+      <Cat />
+      <Cat />
+    </ul>
+  );
+};
+
+export default CatList;
