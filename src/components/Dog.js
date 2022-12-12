4314041a3b15d7710fe9af7ad9ce0d56fe6fb3b3
claire <yue.katou@gmail.com>
2022-12-12 13:29:09 -0800

    React Components

---
+++ src/components/Dog.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dog = () => {
+  return <h3>Dog Component</h3>;
+};
+
+export default Dog;
---
+++ src/components/DogList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DogList = () => {
+  return <h1>DogList Component</h1>;
+};
+
+export default DogList;
