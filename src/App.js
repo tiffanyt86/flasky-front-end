9f73dec8a27b3cb679de1e66653eebd04fa48243
Jayce Mitchell <jayce.mitchell@adadevelopersacademy.org>
2022-12-13 10:23:39 -0800

    Adding props to Dog Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import "./App.css";
 import Dog from "./components/Dog.js";
 import DogList from "./components/DogList";
 
+const dogData = [
+  {
+    name: "Wishbone",
+    favoriteToy: "Squeaker",
+    chipNumber: "82373",
+  },
+  {
+    name: "Appa",
+    favoriteToy: "Ball",
+    chipNumber: "29238",
+  },
+  {
+    name: "Jimminy",
+    favoriteToy: "Bone",
+    chipNumber: "72902",
+  }
+];
+
+const specialDog = dogData[0];
+
 function App() {
   return (
     <div className='App'>
       <DogList />
-      <Dog />
+      <Dog
+        name={specialDog.name}
+        favoriteToy={specialDog.favoriteToy}
+        chipNumber={specialDog.chipNumber}
+      />
     </div>
   );
 }
--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,7 +1,22 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const Dog = () => {
-  return <h3>Dog Component</h3>;
+const Dog = (props) => {
+  return (
+    <>
+      <h3>Dog: {props.name}</h3>
+      <ul>
+        <li>Favorite Toy: {props.favoriteToy}</li>
+        <li>Chip Number: {props.chipNumber}</li>
+      </ul>
+    </>
+  );
+};
+
+Dog.propTypes = {
+  name: PropTypes.string.isRequired,
+  favoriteToy: PropTypes.string.isRequired,
+  chipNumber: PropTypes.string.isRequired
 };
 
 export default Dog;
