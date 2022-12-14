bbec9169021d3ba6f0f3cbfee080f2eb277c2560
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2022-12-14 11:13:15 -0800

    State and Event Handling: Add pet click counter to the cat component

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Cat = (props) => {
+  const [petCount, setPetCount] = useState(0);
+
+  const increasePets = () => {
+    setPetCount(petCount => petCount + 1);
+  };
+
   return (
     <li>
       <h3>Cat name: {props.name}</h3>
       <h3>Personality: {props.personality}</h3>
       <h3>Color: {props.color}</h3>
       <h3>Caretaker: {props.caretaker}</h3>
-      <button>Pet Cat</button>
+      <h3># Pets: {petCount}</h3>
+      <button onClick={(e) => increasePets()}>Pet Cat</button>
     </li>
   )
 };
