6b38fd74379e5ba6851e551d07f7c37c0133a8c3
Tiffany Tran <tif.tran86@gmail.com>
2022-12-21 20:56:21 -0800

    small edit

--- src/components/NewCatForm.js
+++ src/components/NewCatForm.js
@@ -1,19 +1,18 @@
-mport React, { useState } from 'react';
+import React, { useState } from "react";
 
 const kDefaultFormState = {
-  name: "", 
-  personality: "", 
-  color: ""
+  name: "",
+  personality: "",
+  color: "",
 };
 
 const NewCatForm = ({ handleCatSubmit }) => {
-
   const [formData, setFormData] = useState(kDefaultFormState);
-  
+
   const handleChange = (event) => {
     const fieldValue = event.target.value;
     const fieldName = event.target.name;
-    const newFormData = {...formData, [fieldName]: fieldValue}
+    const newFormData = { ...formData, [fieldName]: fieldValue };
 
     setFormData(newFormData);
   };
@@ -22,25 +21,45 @@ const NewCatForm = ({ handleCatSubmit }) => {
     event.preventDefault();
     handleCatSubmit(formData);
     setFormData(kDefaultFormState);
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="name">Cat Name: </label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="personality">Cat Personality: </label>
-        <input type="text" id="personality" name="personality" value={formData.personality} onChange={handleChange}/>
+        <input
+          type="text"
+          id="personality"
+          name="personality"
+          value={formData.personality}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="color">Cat Color: </label>
-        <input type="text" id="color" name="color" value={formData.color} onChange={handleChange}/>
+        <input
+          type="text"
+          id="color"
+          name="color"
+          value={formData.color}
+          onChange={handleChange}
+        />
       </div>
-      <div><input type="submit" value="Add a Cat" /></div>
+      <div>
+        <input type="submit" value="Add a Cat" />
+      </div>
     </form>
-  )
-}
+  );
+};
 
 export default NewCatForm;
